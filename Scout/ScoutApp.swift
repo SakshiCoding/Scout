fd09786df128396c4653ac585ae6ed5c8ebf5af7
SakshiSanganiQudrant <sakshi.s@quadranttechnologies.com>
2026-05-12 12:22:52 -0700

    Initial Commit

---
+++ Scout/ScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutApp.swift
+//  Scout
+//
+//  Created by Sakshi Sangani on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
